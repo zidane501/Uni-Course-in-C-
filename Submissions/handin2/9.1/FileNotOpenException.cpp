da00ca05cf9f4fd779c65ff7b25ecd3c9da05d92
zidane501 <laudm82@gmail.com>
2019-05-27 18:09:51 +0200

    9.1 and thereby assignment 2 done

---
+++ Submissions/handin2/9.1/FileNotOpenException.cpp
@@ -0,0 +1,5 @@
+#include "FileNotOpenException.hpp"
+
+FileNotOpenException::FileNotOpenException(std::string prob):Exception("FILE", prob){
+
+}
---
+++ Submissions/handin2/9.1/OutOfRangeException.cpp
@@ -0,0 +1,5 @@
+#include "OutOfRangeException.hpp"
+
+OutOfRangeException::OutOfRangeException(std::string prob):Exception("Out Of Range", prob){
+
+}

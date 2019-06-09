10b87d03df55eb9264921935b32bb91374e8c3e4
zidane501 <laudm82@gmail.com>
2019-06-09 04:45:39 +0200

    finished with nearest neighbour, yay!

--- Submissions/handin4/main.cpp
+++ Submissions/handin4/main.cpp
@@ -11,8 +11,10 @@ void test_ReadFile(){
 
     int cols = 34;
     int nRowsX = 200;
-    int nRowsXtest = 151;
+    int nRowsXtest = 200; //151;
     int guess = 0;
+    int correct = 0;
+    int True = -1;
 
     double distance;
 
@@ -27,20 +29,23 @@ void test_ReadFile(){
     arma::mat dataXtest(nRowsXtest,cols);
     
     Read.fillMatrix(dataX, "dataX.dat");
-    Read.fillMatrix(dataXtest, "dataXtest.dat");    
+    Read.fillMatrix(dataXtest, "dataX.dat");    
     Read.fillMatrix(labelsY, "dataY.dat");
 
     for (int rowTest = 0; rowTest < nRowsXtest; rowTest++)
     {
         for (int rowX = 0; rowX < nRowsX; rowX++)
-        {
+        {   
+            // calc vector between point and other point
             rowdX = dataXtest.row(rowTest) - dataX.row(rowX);
-            distance = norm(rowdX);
-            //std::cout << "norm: " << distance << std::endl;
 
-            distLabelVec.push_back(distance);
-            distLabelVec.push_back(labelsY(rowX,0));
+            distance = norm(rowdX);
+            
+            // Create vector with relevant info of the point close to it (distance, label, index)
+            distLabelVec.push_back(distance); 
+            distLabelVec.push_back(labelsY(rowTest,0));
             distLabelVec.push_back((double)rowTest);
+            
             allVec.push_back(distLabelVec);
 
             distLabelVec.clear();
@@ -50,6 +55,8 @@ void test_ReadFile(){
         std::sort(allVec.begin(), allVec.end(), [](const std::vector<double>& a, const std::vector<double>& b){ return a[0] < b[0]; } );
         
         guess = 0;
+
+        
         // calc label (sum top 5 of allvec)
         for (int i = 0; i < 5; i++){
             
@@ -60,19 +67,24 @@ void test_ReadFile(){
             }
             
         }
-        
-        allVec.clear();
 
-        std::cout << "guess: " << guess/std::abs(guess) << ", " << rowTest + 1 << std::endl;
+        True = std::pow(-1, rowTest); // To test how many right results
+
         
-        if (guess>0){
-            guessLabels.push_back(1);
-        }else{
-            guessLabels.push_back(-1);
-        }      
+        correct += labelsY(rowTest,0)==(guess/std::abs(guess)); // To test how many right results
+        
+        allVec.clear(); // reset allVec to reuse it 
+
+        std::cout << "guess: " << guess/std::abs(guess) << ", " <<  labelsY(rowTest,0) << " (" << rowTest + 1 << ")" << std::endl;
+        
+        //
+        
+        guessLabels.push_back(guess/std::abs(guess)); // Set labels
+
     }
 
-
+    std::cout << "correct: " << correct << std::endl; // Print correct number out of 200
+    
     for (size_t i = 0; i < nRowsX; i++){
 
         //std::cout << "labelsY[" << i << "]: " << labelsY[i] << " | guessLabels[" << i << "]: " << guessLabels[i] << std::endl;
@@ -81,7 +93,8 @@ void test_ReadFile(){
 }
 
 int main(int argc, char const *argv[])
-{
+{   
+    // test
     arma::mat A = arma::randu<arma::mat>(4,5);
     arma::mat B = arma::randu<arma::mat>(4,5);
   
